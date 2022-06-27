25077f4a7d3859e79b6ee50f9e8a7ae6fdf23741
Michael Poon <michaelkm.poon@mail.utoronto.ca>
2022-06-26 23:23:43 -0400

    first working WH integrator

--- vanillaWH.py
+++ vanillaWH.py
@@ -1,4 +1,4 @@
-import rebound 
+import rebound #bruh
 # import matplotlib.pyplot as plt
 import numpy as np
 import time
@@ -10,13 +10,13 @@ def drift(simj, sim, object_num, h):
     Advance (Keplerian part) system by timestep h
     
     Parameters:
-        simj (array): [x, y, vx, vy, m] stacked for each particle in Jacobi coordinates
-        sim (array): [x, y, vx, vy, m] stacked for each particle in inertial coordinates
+        simj (array): [x, y, z, vx, vy, vz, m] stacked for each particle in Jacobi coordinates
+        sim (array): [x, y, z, vx, vy, vz, m] stacked for each particle in inertial coordinates
         object_num (int): index of orbiting particle
         h (float): timestep
 
     Returns:
-        (array): [x, y, vx, vy, m] stacked for each updated particle in Jacobi coodinates  
+        (array): [x, y, z, vx, vy, vz, m] stacked for each updated particle in Jacobi coodinates  
     """
     
     r = simj[object_num, :3]
@@ -118,6 +118,116 @@ def calculate_c(n, z, tolerance=1e-14, max_j=30):
         
     return next_c
 
+def kick(simj, sim, h):
+    """
+    Advance (Interaction part) system by timestep h
+    
+    Parameters:
+        simj (array): [x, y, z, vx, vy, vz, m] stacked for each particle in Jacobi coordinates
+        sim (array): [x, y, z, vx, vy, vz, m] stacked for each particle in inertial coordinates
+        h (float): timestep
+
+    Returns:
+        (array): [x, y, z, vx, vy, vz, m] stacked for each updated particle in Jacobi coodinates  
+    """
+
+    # (1/4) Calculate x, y, z accelerations from first part of the Interaction Hamiltonian (in Jacobi coords)
+    # if i >= 1:
+    #     a_{i,Jac,x} = Mi * ri_x / norm (ri)^3
+    #     a_{i,Jac,y} = Mi * ri_y / norm (ri)^3
+    #     a_{i,Jac,z} = Mi * ri_z / norm (ri)^3
+    
+    acceleration1j = np.zeros((len(simj), 3))
+    
+    Mi = 0.
+    Mi += sim[0, 6] + sim[1, 6]
+
+    for i in range(2, len(simj)):
+
+        mi = sim[i, 6]
+        Mi += mi
+
+        # x, y, z in jacobi coordinates for the ith object 
+        rij_x = simj[i, 0]
+        rij_y = simj[i, 1]
+        rij_z = simj[i, 2]
+        rij3 = np.sqrt(rij_x**2 + rij_y**2 + rij_z**2)**3
+
+        acceleration1j[i, 0] = Mi * (rij_x / rij3)
+        acceleration1j[i, 1] = Mi * (rij_y / rij3) 
+        acceleration1j[i, 2] = Mi * (rij_z / rij3) 
+
+    # (2/4) Calculate k-th x, y, z accelerations from second part of the Interaction Hamiltonian (in inertial coords)
+    # 
+    # a_{k,inertial,x} = Sum (from k=0 to N-1, j=k+1 to N-1) of mk * mj * (rj_x - rk_x) / norm (rk - rj)^3
+    #                  + Sum (from i=0 to N-1, k=i+1 to N-1) of mi * mk * (rk_x - rj_x) / norm (ri - rk)^3
+
+    acceleration2 = np.zeros((len(simj), 3))
+    
+    for k in range(len(simj)):
+        
+        for j in range(k+1, len(simj)):
+            
+            if j != 1:
+                
+                mj = sim[j, 6]
+
+                # here, j is the jth term, not Jacobi coordinate
+                rk_x, rj_x = sim[k, 0], sim[j, 0] 
+                rk_y, rj_y = sim[k, 1], sim[j, 1] 
+                rk_z, rj_z = sim[k, 2], sim[j, 2] 
+                dx = rk_x - rj_x
+                dy = rk_y - rj_y
+                dz = rk_z - rj_z
+                rk_minus_rj3 = np.sqrt(dx**2 + dy**2 + dz**2)**3
+
+                acceleration2[k, 0] += mj * (rj_x-rk_x) / rk_minus_rj3
+                acceleration2[k, 1] += mj * (rj_y-rk_y) / rk_minus_rj3
+                acceleration2[k, 2] += mj * (rj_z-rk_z) / rk_minus_rj3
+            
+    for i in range(len(simj)):
+        
+        for k in range(i+1, len(simj)):
+            
+            if k != 1:
+
+                mi = sim[i, 6]
+
+                ri_x, rk_x = sim[i, 0], sim[k, 0] 
+                ri_y, rk_y = sim[i, 1], sim[k, 1] 
+                ri_z, rk_z = sim[i, 2], sim[k, 2] 
+                dx = ri_x - rk_x
+                dy = ri_y - rk_y
+                dz = ri_z - rk_z
+                ri_minus_rk3 = np.sqrt(dx**2 + dy**2 + dz**2)**3
+
+                acceleration2[k, 0] -= mi * (rk_x-ri_x) / ri_minus_rk3
+                acceleration2[k, 1] -= mi * (rk_y-ri_y) / ri_minus_rk3
+                acceleration2[k, 2] -= mi * (rk_z-ri_z) / ri_minus_rk3
+            
+    # (3/4) Convert accelerations from second part of the Interaction Hamiltonian to Jacobi coords
+    # follow same as posvel conversion (does not rely on posvel)
+    
+    acceleration2j = inertial_to_jacobi_acc(sim, acceleration2)
+    
+    # (4/4) kick Jacobi velocities by h * (accel_part1 + accel_part2)
+    
+    for i in range(len(simj)):
+        
+        #if i == 2:
+        #    print(i, acceleration1j[i, 0])
+        #    print(i, acceleration1j[i, 1])
+        
+        simj[i, 3] += h * (acceleration1j[i, 0] + acceleration2j[i, 0]) 
+        simj[i, 4] += h * (acceleration1j[i, 1] + acceleration2j[i, 1]) 
+        simj[i, 5] += h * (acceleration1j[i, 2] + acceleration2j[i, 2]) 
+        
+        #simj[i, 3] += h * (acceleration2j[i, 0]) 
+        #simj[i, 4] += h * (acceleration2j[i, 1]) 
+        #simj[i, 5] += h * (acceleration2j[i, 2]) 
+        
+    return simj
+    
 def move_to_com(sim):
     """
     Convert from heliocentric to inertial (centre-of-mass) positions and velocities
@@ -208,7 +318,7 @@ def inertial_to_jacobi(sim):
         vi_y = sim[i, 4]
         vi_z = sim[i, 5]
         
-        # x, y, vx, vy in jacobi coordinates for the ith object 
+        # x, y, z, vx, vy, vz in jacobi coordinates for the ith object 
         rij_x = ri_x - COM_x/M_iminus1
         rij_y = ri_y - COM_y/M_iminus1
         rij_z = ri_z - COM_z/M_iminus1
@@ -258,6 +368,67 @@ def inertial_to_jacobi(sim):
     simj[0, 6] = m0j
     
     return simj
+
+def inertial_to_jacobi_acc(sim, acceleration_list):
+    """
+    Convert from inertial (centre-of-mass) accelerations to Jacobi accelerations
+    
+    This follows transformations.c from Rebound, WHFast paper, and Mikkola & Innanen 1999
+    
+    Parameters:
+        sim (array): [x, y, z, vx, vy, vz, m] stacked for each particle in inertial coordinates
+        acceleration_list (array): list of accelerations from 2nd part of Interaction Hamiltonian
+        
+    Returns:
+        (array): list of Jacobi accelerations
+    """
+    
+    accelerationj = np.zeros((len(acceleration_list), 3))
+    
+    m0 = sim[0, 6]
+    a0 = acceleration_list[0]
+    
+    COM_ax = m0 * a0[0]
+    COM_ay = m0 * a0[1]
+    COM_az = m0 * a0[2]
+
+    M_iminus1 = 0.
+
+    for i in range(1,len(sim)):
+        
+        mi = sim[i, 6]
+        
+        m_iminus1 = sim[i-1, 6]
+        M_iminus1 += m_iminus1
+        
+        ai_x = acceleration_list[i, 0]
+        ai_y = acceleration_list[i, 1]
+        ai_z = acceleration_list[i, 2]
+
+        # ax, ay, az in jacobi coordinates for the ith object 
+        aij_x = ai_x - COM_ax/M_iminus1
+        aij_y = ai_y - COM_ay/M_iminus1
+        aij_z = ai_z - COM_az/M_iminus1
+        
+        accelerationj[i, 0] = aij_x
+        accelerationj[i, 1] = aij_y
+        accelerationj[i, 2] = aij_z
+        
+        COM_ax = COM_ax * (1 + mi/M_iminus1) + mi*aij_x
+        COM_ay = COM_ay * (1 + mi/M_iminus1) + mi*aij_y
+        COM_az = COM_az * (1 + mi/M_iminus1) + mi*aij_z
+    
+    # Different convention for 0-th coordinate
+    m_total = M_iminus1 + mi
+    a0j_x = COM_ax / m_total
+    a0j_y = COM_ay / m_total
+    a0j_z = COM_az / m_total
+    
+    accelerationj[0, 0] = a0j_x
+    accelerationj[0, 1] = a0j_y
+    accelerationj[0, 2] = a0j_z
+    
+    return accelerationj
 
 def jacobi_to_inertial(simj, sim):
     """
@@ -348,14 +519,14 @@ def jacobi_to_inertial(simj, sim):
     return sim
 
 def energy_fn(sim):
-    """ Calculate energy per unit mass as shown in p. 77 of Tremaine's book
+    """ Calculate energy of system as shown in p. 77 of Tremaine's book
     
     Parameters:
         sim (rebound.simulation.Simulation): 
             Rebound simulation object
 
     Returns:
-        (float): Energy per unit mass
+        (float): Total energy of the system 
     """
    
     e_kin = 0.
@@ -366,7 +537,8 @@ def energy_fn(sim):
         mi = sim[i,6]
         xi, yi, zi = sim[i,0], sim[i,1], sim[i,2]
         vxi, vyi, vzi = sim[i,3], sim[i,4], sim[i,5]
-        e_kin += 0.5 * m * (vxi**2 + vyi**2 + vzi**2)
+        
+        e_kin += 0.5 * mi * (vxi**2 + vyi**2 + vzi**2)
         
         for j in range(len(sim)):
             
@@ -376,6 +548,6 @@ def energy_fn(sim):
                 dx = xi - xj
                 dy = yi - yj
                 dz = zi - zj
-                e_pot += - sim.G * m / np.sqrt(dx*dx + dy*dy + dz*dz)
+                e_pot += - mi * mj / np.sqrt(dx*dx + dy*dy + dz*dz)
     
     return e_kin + e_pot
